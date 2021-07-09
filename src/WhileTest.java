c42c0e600faace23bc7a3f8a429ebfd950f1f86e
编程 <1660873558@qq.com>
2021-07-09 17:34:27 +0800

    while循环基本使用

---
+++ src/WhileTest.java
@@ -0,0 +1,15 @@
+public class WhileTest {
+    public static void main (String[] args){
+
+        //遍历100以内的所有偶数
+        int i = 1;
+        while (i <= 100){
+
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+            i++;
+        }
+        System.out.println(i);//100? 101
+    }
+}

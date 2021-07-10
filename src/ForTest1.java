2652062d7233c9b34e6c2c7599ed417e3eb2cda5
编程 <1660873558@qq.com>
2021-07-10 08:36:38 +0800

    循环打印九九乘法表1的倍数

---
+++ src/ForTest1.java
@@ -0,0 +1,21 @@
+public class ForTest1 {
+
+    public static void main(String[] args) {
+        //for (int i = 1; i < 10; i++){
+           // System.out.println("1 * " + i + " = " + 1 * i + "");
+
+        //int i = 1;
+        //while ( i < 10){
+        //    System.out.println("1 * " + i + " = " + 1 * i + "");
+        //    i++;
+
+        int i = 1;
+        do {
+            System.out.println("1 * " + i + " = " + 1 * i + "");
+            i++;
+        }while (i < 10);
+    }
+}
+
+
+
